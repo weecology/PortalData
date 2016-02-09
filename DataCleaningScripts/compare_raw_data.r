b191918934dbacf36d81a88e124055f1993c7711
= <erica.christensen@weecology.org>
2016-02-09 10:47:41 -0500

    added comments; moved file loading inside function; added code to output column name as well as row number of error

--- DataCleaningScripts/compare_raw_data.r
+++ DataCleaningScripts/compare_raw_data.r
@@ -1,55 +1,68 @@
-# Problems:
-#  
-
-
-
 # This script takes data input from two different users in two sheets of an excel 
 # workbook and prints a list of rows that are not identical, for review by the user.
+# Output is a data frame of row and column of a mismatch.
 
-# Note: name of excel file should be changed in line 17 of this script
+# Notes: row order matters, i.e. data sheets must be entered in same order
+#        worksheets to be compared must be the first two worksheets in the workbook
+#        name of worksheets does not matter
+#        returns "NULL" if two worksheets are identical
 
-
-
-rm(list=ls(all=TRUE))
 
 library(XLConnect)
 
-filename = 'newdat445'
-filepath = 'C:/Users/ellen.bledsoe/Dropbox/Portal/PORTAL_primary_data/Rodent/Raw_data/New_data/'
-
 # ===============================================================================
-# functions
+# Functions
 
-compare_by_row = function(ws1,ws2) {
-  # this function compares the two excel worksheets row by row to identify inconsistencies
-  num_rows = length(ws1$mo)
-  unmatched = vector()
-  curr_row = 1
-  while (curr_row<=num_rows) {
-    row1 = as.character(as.vector(ws1[curr_row,]))              #extract single row
-    row1[is.na(row1)] = ''                                      #remove NAs
-    row2 = as.character(as.vector(ws2[curr_row,]))
-    row2[is.na(row2)] = ''
-    if (!all(row1==row2)) {
-      unmatched = append(unmatched,curr_row+1)
+compare_worksheets = function(excel_file) {
+  
+  # this function compares the two excel worksheet to identify inconsistencies
+  
+  # load data from excel workbook
+  wb = loadWorkbook(excel_file)
+  ws1 = readWorksheet(wb, sheet = 1, header = TRUE,colTypes = XLC$DATA_TYPE.STRING)
+  ws2 = readWorksheet(wb, sheet = 2, header = TRUE,colTypes = XLC$DATA_TYPE.STRING)
+  
+  
+  # if the two worksheets are identical, exit function
+  if (identical(ws1,ws2)) {                                   
+    return(unmatched = c())
+  } 
+  
+  # otherwise, loop through rows one at a time
+  else {
+      unmatched = data.frame(row = c(),column = c())         # empty data frame for storing output
+      num_rows = length(ws1$mo)
+      curr_row = 1
+      while (curr_row<=num_rows) {
+        v1 = as.character(as.vector(ws1[curr_row,]))          # extract row from worksheet 1
+        v2 = as.character(as.vector(ws2[curr_row,]))          # extract row from worksheet 2
+        
+        # if the two versions of the row are not identical
+        if (!identical(v1,v2)) {
+          # loop through each element in the row
+          col_error = vector()
+          for (n in seq(length(v1))) {                        
+            if (!identical(v1[n],v2[n])) {
+              # add the column name to output vector
+              col_error = append(col_error,colnames(ws1)[n])
+            }
+          }
+          # append row and column info to output data frame (curr_row+1 to skip header in excel file)
+          unmatched = rbind(unmatched,data.frame(row = curr_row+1,column = col_error))
+        }
+        curr_row = curr_row+1             # increment index and continue loop
+      }
+      return(unmatched)
     }
-    curr_row = curr_row+1
-  }
-  return(unmatched)
 }
 
 # ================================================================================
-# data files
+# Main code
+
+filename = 'newdat445'
+filepath = 'C:/Users/EC/Dropbox/Portal/PORTAL_primary_data/Rodent/Raw_data/New_data/'
 
 newfile = paste(filepath, filename, '.xlsx', sep='')
 
-# ===============================================================================
-# compare two copies of input data
-
-wb = loadWorkbook(newfile)
-ws1 = readWorksheet(wb, sheet = 1, header = TRUE,colTypes = XLC$DATA_TYPE.STRING)
-ws2 = readWorksheet(wb, sheet = 2, header = TRUE,colTypes = XLC$DATA_TYPE.STRING)
-
-unmatched = compare_by_row(ws1,ws2)
-print(c('unmatched rows:',unmatched))
+compare_worksheets(newfile)
 
