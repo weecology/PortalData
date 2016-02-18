3fc3629ff515703142f51a44d745ff21a90e72ad
= <erica.christensen@weecology.org>
2016-02-18 11:35:05 -0500

    added function to compare written tag numbers to numbers downloaded from scanner

--- DataCleaningScripts/new_rodent_data.r
+++ DataCleaningScripts/new_rodent_data.r
@@ -4,17 +4,24 @@
 
 
 source('compare_raw_data.r')
+source('compare_tags.r')
 
 # ============================================================================
 # New file to be checked
 
-filename = 'newdat445'
+newperiod = '445'
 filepath = 'C:/Users/EC/Dropbox/Portal/PORTAL_primary_data/Rodent/Raw_data/New_data/'
 
-newfile = paste(filepath, filename, '.xlsx', sep='')
-
+newfile = paste(filepath, 'newdat', newperiod, '.xlsx', sep='')
+scannerfile = paste(filepath, 'tag scans/tags', newperiod, '.txt', sep='')
 
 # ============================================================================
-# Compare double-entered data -- will return 'NULL' if versions match
+# Compare double-entered data -- will return 'Worksheets identical' if versions match
 
 compare_worksheets(newfile)
+
+# ============================================================================
+# General error checking
+
+# Compare tag numbers entered on sheets to scanner download
+compare_tags(newfile,scannerfile)
