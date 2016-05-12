0df1edbbad46d52fd45c40028167c263764b4a7a
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2016-05-12 11:07:26 -0400

    Add quality control and appending data to script

--- DataCleaningScripts/clean_shrub_transect_data.R
+++ DataCleaningScripts/clean_shrub_transect_data.R
@@ -10,8 +10,14 @@
 #       a. Source code from rodent checking (EMC)
 #   Section 3
 #     * a. Ensure that species list path is correct *
+#   Section 4
+#       a. ONLY DO THIS ONCE!
+#     * b. Ensure the file path is correct *
+
+#####################################################################################
 
 library(XLConnect)
+library(dplyr)
 
 source('compare_raw_data.r')
 
@@ -35,12 +41,13 @@ compare_worksheets(newfile)
 # 3. Quality control #
 ######################
 
-wb = loadWorkbook(newfile)
-ws = readWorksheet(wb, sheet = 1, header = TRUE,colTypes = XLC$DATA_TYPE.STRING)
+wb <-  loadWorkbook(newfile)
+ws <-  readWorksheet(wb, sheet = 1, header = TRUE,colTypes = XLC$DATA_TYPE.STRING)
 
-splist = read.csv('C:/Users/ellen.bledsoe/Desktop/Git/PortalData/Plants/Portal_plant_species.csv', as.is = T)
+splist <-  read.csv('C:/Users/ellen.bledsoe/Desktop/Git/PortalData/Plants/Portal_plant_species.csv', as.is = T)
 
-plots = seq(24)
+plots <-  1:24
+transects <- c("T1", "T2")
 
 # =====================================
 # species names not in official list
@@ -48,10 +55,30 @@ plots = seq(24)
 unique(ws$species[!(ws$species %in% splist$Sp.Code)])
 
 # =====================================
-# are all quadrats present
+# are all transects present
 
-# changes to all plots present and all plot/transect combos present
+all_trans <-  apply(expand.grid(plots, transects), 1, paste, collapse = ' ') %>% trimws()
+plot_trans <-  unique(paste(ws$plot, ws$transect))
 
-allquads <-  apply(expand.grid(plots,stakes),1,paste,collapse=' ')
-plotquad <-  unique(paste(ws$plot,ws$stake))
+# any plot-transect pairs that should be censused that are not in the data
+setdiff(all_trans, plot_trans)
+
+# =====================================
+# save cleaned up version to Dropbox
+
+# make sure you are saving the most up-to-date version of the file
+ws <-  readWorksheet(wb, sheet = 1, header = TRUE,colTypes = XLC$DATA_TYPE.STRING)
+
+write.csv(ws, file = paste(filepath, "ShrubTransect_", season, year, "_clean", ".csv", sep = ''), 
+          row.names = FALSE, na = "")
+
+#################################################
+# 4. Append new data to 2015+ plant data in Git #
+#################################################
+
+data_append <- data_clean[, c("year", "month", "day", "transect", "plot", "location", "species", "height", "notes")]
+
+# append to existing data file
+write.table(data_append, file = "C:/Users/ellen.bledsoe/Desktop/Git/PortalData/Plants/Portal_plant_transects_2015_present.csv", 
+            row.names = F, col.names = F, na = "", append = TRUE, sep = ",")
 
