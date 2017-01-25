541e1091c734e67a14788c212cdfa7c2e2d34de2
Glenda <glenda@weecology.org>
2017-01-25 14:08:31 -0700

    COrrect assignment of treatment type over time

--- DataSummaryScripts/RodentAbundances.R
+++ DataSummaryScripts/RodentAbundances.R
@@ -1,15 +1,17 @@
 #A function to summarize monthly species abundance
 #with options to summarize by "Plot", "Treatment", or "Site" (level)
+#to use all plots or only plots that have had the same treatment over the entire time series
+#length="Longterm"
 #and to include all "Rodents" or only "Granivores" (type)
 
-library(dtplyr)
 library(dplyr)
 library(tidyr)
 
-abundance <- function(level,type) {
+abundance <- function(level,type,length) {
 
 rodents=read.csv('~/PortalData/Rodents/Portal_rodent.csv', na.strings=c(""), colClasses=c('tag'='character'), stringsAsFactors = FALSE)
 species=read.csv('~/PortalData/Rodents/Portal_rodent_species.csv',na.strings=c(""))
+plots=read.csv('~/PortalData/SiteandMethods/Portal_plots.csv')
 colnames(species)[1]="species"
   
 
@@ -26,21 +28,15 @@ rodents = rodents %>%
   left_join(species,rodents, by="species") %>%
   filter(Rodent==1, Unidentified==0)
 
+###########Use only Long-term treatments --------------
+if(length %in% c("Longterm","longterm")) {rodents = rodents %>% filter(plot %in% c(3,4,6,10,11,14,15,16,17))}
 
 ###########Summarise by Treatment ----------------------
-if(level=="Treatment"){
+if(level %in% c("Treatment","treatment")){
 #Name plot treatments in each time period
-removal=c(2,4,8,11,12,14,17,22) #removals
-exclosure=c(3,6,13,18,19,20) #krat exclosures
 
-removal2=c(2,4,8,11,12,14,17,22) #removals post March 2015
-exclosure2=c(3,6,13,18,19,20) #krat exclosures post March 2015
-
-rodents$treatment = "control"
-rodents$treatment[rodents$plot %in% removal] = "removal"
-rodents$treatment[rodents$plot %in% exclosure] = "kratexclosure"
-
-
+rodents = left_join(rodents,plots)
+  
 abundances = rodents %>% 
   mutate(species = factor(species)) %>% 
   group_by(period,treatment) %>%
@@ -50,8 +46,9 @@ abundances = rodents %>%
 }
 
 ##########Summarise by plot -----------------
-if(level=="Plot"){
-abundances = rodents %>% 
+if(level %in% c("Plot","plot")){
+
+  abundances = rodents %>% 
   mutate(species = factor(species)) %>% 
   group_by(period,plot) %>%
   do(data.frame(x = table(.$species))) %>% 
@@ -60,8 +57,9 @@ abundances = rodents %>%
 }
 
 ##########Summarise site-wide -----------------
-if(level=="Site"){
-abundances = rodents %>% 
+if(level %in% c("Site","site")){
+
+  abundances = rodents %>% 
   mutate(species = factor(species)) %>% 
   group_by(period) %>%
   do(data.frame(x = table(.$species))) %>% 
@@ -70,7 +68,7 @@ abundances = rodents %>%
 }
 
 ###########Exclude non-granivores---------------
-if(type=="Granivores"){
+if(type %in% c("Granivores","granivores")){
   rodents = rodents %>%
     left_join(species,rodents, by="species") %>%
     filter(Granivore==1)
