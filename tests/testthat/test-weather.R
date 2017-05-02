168b6892c93afa3317a6aae1785120e140c39821
Glenda <glenda@weecology.org>
2017-05-02 13:51:31 -0600

    Pull weather data from Dropbox link, test, update weather

--- tests/testthat/test-weather.R
+++ tests/testthat/test-weather.R
@@ -1,55 +1,48 @@
-source("~/PortalData/DataCleaningScripts/new_weather_data.R")
+source("new_weather_data.R")
 library(testthat)
 context("checks new weather data")
 
+data=new_met_data()
+newdata = data[[1]]
+newdata_cols = colnames(newdata)
+weather = data[[2]]
+weather_cols = colnames(weather)
+stormsnew = data[[3]]
+stormsnew_cols = colnames(stormsnew)
+storms = data[[4]]
+storms_cols = colnames(storms)
 
 test_that("required column names in new weather df", {
-  data=new_met_data()
-  newdata = data[[1]]
-  newdata_cols = colnames(newdata)
-  weather = data[[2]]
-  weather_cols = colnames(weather)
-  stormsnew = data[[3]]
-  stormsnew_cols = colnames(stormsnew)
-  storms = data[[4]]
-  storms_cols = colnames(storms)
   
   expect_identical(weather_cols, newdata_cols)
   expect_identical(storms_cols, stormsnew_cols)
 })
 
 test_that("Hour in 100:2400", {
-  data=new_met_data()
-  newdata = data[[1]]
+  
   expect_true(sum(newdata$Hour %in% seq(from=100,to=2400,by=100))==dim(newdata)[1])
 })
 
 test_that("Air Temperature ok", {
-  data=new_met_data()
-  newdata = data[[1]]
+
   expect_true(sum(newdata$TempAir < -30)==0)
   expect_true(sum(newdata$TempAir > 100)==0)
 })
 
 test_that("Relative humidity ok", {
-  data=new_met_data()
-  newdata = data[[1]]
+
   expect_true(sum(newdata$RH < 0)==0)
   expect_true(sum(newdata$RH > 100)==0)
 })
 
 test_that("battery status ok", {
-  data=new_met_data()
-  newdata = data[[1]]
-  stormsnew = data[[3]]
+  
   expect_true(sum(newdata$BattV < 9)==0)
   expect_true(sum(stormsnew$BattV < 9) ==0)
 })
 
 test_that("Precipitation ok", {
-  data=new_met_data()
-  newdata = data[[1]]
-  stormsnew = data[[3]]
+  
   expect_true(sum(newdata$Precipitation < 0)==0)
   expect_true(sum(newdata$Precipitation > 100)==0)
   expect_true(sum(stormsnew$Rain_mm_Tot < 0) ==0)
@@ -57,42 +50,30 @@ test_that("Precipitation ok", {
 })
 
 test_that("Precipitation in multiples of 0.254", {
-  data=new_met_data()
-  newdata = data[[1]]
-  stormsnew = data[[3]]
+  
   expect_true(sum(newdata$Precipitation%%0.254)==0)
   expect_true(sum(stormsnew$Rain_mm_Tot%%0.254)==0)
 })
 
 
 test_that("start of new data lines up with end of existing data", {
-  data=new_met_data()
-  newdata = data[[1]]
-  weather = data[[2]]
-  stormsnew = data[[3]]
-  storms = data[[4]]
+  
   expect_identical(tail(weather$TIMESTAMP,n=1)+3600,newdata$TIMESTAMP[1])
 })
 
 test_that("no hours missing", {
-  data=new_met_data()
-  newdata = data[[1]]
-  weather = data[[2]]
+  
   expect_true(sum(diff(newdata$TIMESTAMP)!=1)==0)
 })
 
 test_that("no identical rows in newdata and weather", {
-  data=new_met_data()
-  newdata = data[[1]]
-  weather = data[[2]]
-  expect_true(sum(duplicated(bind_rows(weather,newdata)))==0)
+  
+  expect_true(sum(duplicated(dplyr::bind_rows(weather,newdata)))==0)
 })
 
 test_that("no identical rows in new storm data and storms", {
-  data=new_met_data()
-  newdata = data[[1]]
-  weather = data[[2]]
-  expect_true(sum(duplicated(bind_rows(storms,stormsnew)))==0)
+  
+  expect_true(sum(duplicated(dplyr::bind_rows(storms,stormsnew)))==0)
 })
 
 
