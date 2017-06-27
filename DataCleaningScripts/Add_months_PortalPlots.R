38ab89ea7c61befb85ed528af5022707fa81823d
Glenda <glenda@weecology.org>
2017-06-27 12:56:54 -0600

    Add tests for Add_months_portalplots.R

--- DataCleaningScripts/Add_months_PortalPlots.R
+++ DataCleaningScripts/Add_months_PortalPlots.R
@@ -1,5 +1,56 @@
+#' Appends new dates to Portal_plots
+#'
+#'
+#' 
+#' @example update_portal_plots()
+#' 
+#'
+#'
+library(zoo)
+update_portal_plots <- function() {
+#load plot data
+portal_plots = read.csv("../SiteandMethods/Portal_plots.csv")
+# load rodent data
+rodentdat = read.csv("../Rodents/Portal_rodent.csv",stringsAsFactors = F,as.is=T,na.strings = '')  
+
+# define currrent plot treatments
+
+controls = c(4,5,6,7,11,13,14,17,18,20,24)
+removals = c(1,9,10,12,16,23)
+exclosures = c(2,3,8,15,19,21,22)
+
+# proceed only if rodentdat has more recent data than plot data
+lastplotdates=max(as.yearmon(paste(substr(portal_plots$yr,3,4),portal_plots$month), "%y%m"))
+newdates=max(as.yearmon(paste(substr(rodentdat$yr,3,4),rodentdat$mo), "%y%m"))
+
+if (newdates > 
+    lastplotdates) {
+  
+  yr = rep(year(newdates),24)
+  month = rep(month(newdates),24)
+  plot = 1:24
+  
+  newplots = data.frame(cbind(yr,month,plot,treatment=NA))
+  
+  newplots = newplots %>% mutate(treatment = ifelse((plot %in% removals),
+                                                                    "removal", treatment)) %>% 
+    mutate(treatment = ifelse((plot %in% exclosures),"exclosure", treatment)) %>% 
+    mutate(treatment = ifelse((plot %in% controls),"control", treatment))
+  
+  portal_plots = bind_rows(portal_plots,newplots)
+}
+
+return(portal_plots)
+}
+
+#Write new portal plot file
+writeportalplots <- function(portal_plots) {
+  write.csv(portal_plots, file='../SiteandMethods/Portal_plots.csv',row.names = F)
+}
+
 ###################################
-### Code to take the original Portal_plots.csv which only reported plot treatment by year
+###This is hw the original Portal_plots.csv was created 
+###Code to take the original Portal_plots.csv which only reported plot treatment by year
 ### And add in what month is was in what treatment
 ### This allows us to more finely control summarizing the Portal data
 ### by being able assign exactly when a plot changed treatment
--- tests/testthat/test-update_portal_plots.R
+++ tests/testthat/test-update_portal_plots.R
@@ -2,17 +2,16 @@ source('Add_months_PortalPlots.r')
 
 library(testthat)
 context("checks that plot assignments are being added correctly")
-portalplots=update_portal_plots()
+portal_plots=update_portal_plots()
 
-test_that("no moondates are skipped or duplicated", {
-  
-  expect_identical(row(as.matrix(moon_dates$NewMoonNumber))[,1],moon_dates$NewMoonNumber)
-  expect_true(all(diff(moon_dates$NewMoonDate) %in% c(29,30)))
+test_that("valid data", {
+  expect_true(all(portal_plots$month %in% 1:12))
+  expect_true(all(portal_plots$plot %in% 1:24))
+  expect_true(all(portal_plots$treatment %in% c("control","removal","exclosure","spectabs")))
 })
 
 
-test_that("no periods skipped or duplicated", {
-  
-  expect_true(all(diff(moon_dates$Period)==1,na.rm=T))
-  expect_true(sum(duplicated(moon_dates$CensusDate,incomparables=NA))==0)
+test_that("no duplicate data", {
+
+  expect_true(sum(duplicated(portal_plots))==0)
 })
