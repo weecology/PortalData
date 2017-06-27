465af43685396a7f4ca1b66778d3d45c886e6b9a
Ethan White <ethan@weecology.org>
2017-06-27 14:48:49 -0400

    Add skeleton for having CI update supplemental data files
    Will automatically update supplemental data files every time new data
    is committed.
    
    Currently this won't do anything because it just runs a file holding a single
    function, which is serving as a place holder to get the CI system up and running.

---
+++ update_repo.sh
@@ -0,0 +1,9 @@
+git config --global user.email "weecologydeploy@weecology.org"
+git config --global user.name "Weecology Deploy Bot"
+
+git checkout master
+git add -u
+git commit -m "Update supplemental data tables: Travis Build $TRAVIS_BUILD_NUMBER"
+
+git remote add deploy https://${GITHUB_TOKEN}@github.com/weecology/portalPredictions.git > /dev/null 2>&1
+git push --quiet deploy master > /dev/null 2>&1
