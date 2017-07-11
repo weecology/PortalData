6fa60c28c7e0ed7fada56611238d0542b5f59c62
Ethan White <ethan@weecology.org>
2017-07-11 15:42:57 -0400

    Push update data tables to correct repo

--- update_repo.sh
+++ update_repo.sh
@@ -5,5 +5,5 @@ git checkout master
 git add -u
 git commit -m "Update supplemental data tables: Travis Build $TRAVIS_BUILD_NUMBER"
 
-git remote add deploy https://${GITHUB_TOKEN}@github.com/weecology/portalPredictions.git > /dev/null 2>&1
+git remote add deploy https://${GITHUB_TOKEN}@github.com/weecology/PortalData.git > /dev/null 2>&1
 git push --quiet deploy master > /dev/null 2>&1
