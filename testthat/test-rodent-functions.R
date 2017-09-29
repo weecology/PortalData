4ab655315f6f523e420a10d142a87bb46e29c7d5
Glenda Yenni <glenda@weecology.org>
2017-09-29 10:23:37 -0700

    Fix more paths

--- testthat/test-rodent-functions.R
+++ testthat/test-rodent-functions.R
@@ -38,3 +38,4 @@ test_that("look for missing data ", {
   expect_equal(check_missing_data(testdat,c('month','day','year','period','plot')),c(5,15))
   expect_equal(check_missing_data(testdat[is.na(testdat$note1),],fields=c('stake','species','sex','hfl','wgt')),11)
 })
+
