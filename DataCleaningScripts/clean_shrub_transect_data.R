e22b139081de2646e1e5fa47763c5af6469447af
Renata Diaz <diaz.renatam@gmail.com>
2017-10-19 16:06:31 -0400

    Cleaning summer 2017 transects

--- DataCleaningScripts/clean_shrub_transect_data.R
+++ DataCleaningScripts/clean_shrub_transect_data.R
@@ -16,35 +16,24 @@
 
 #####################################################################################
 
-library(XLConnect)
+# library(XLConnect) ### Does not work on my computer (RMD).
+library(openxlsx)
 library(dplyr)
 
-source('compare_raw_data.r')
+# source('DataCleaningScripts/compare_raw_data.r')
+
+# First manually compare and edit double-entered data using specific script
+
 
 ######################
-# 1. Load Excel file #
+# 1. Load "clean" .csv file #
 ######################
-
-season <-  'Summer'
-year <-  '2015'
-filepath <-  'C:/Users/ellen.bledsoe/Dropbox/Portal/PORTAL_primary_data/Plant/Transects/ShrubTransects(2015-present)/Rawdata/'
-
-newfile <-  paste(filepath, "ShrubTransect_", season, year, '.xlsx', sep='')
-
-##################################
-# 2. Compare double-entered data #
-##################################
-
-compare_worksheets(newfile)
+ws = read.csv('/Users/renatadiaz/Dropbox/Portal/PORTAL_primary_data/Plant/TRANSECTS/ShrubTransects(2015-present)/RawData/ShrubTransect_Summer2017_clean.csv', stringsAsFactors= F)
 
 ######################
 # 3. Quality control #
 ######################
-
-wb <-  loadWorkbook(newfile)
-ws <-  readWorksheet(wb, sheet = 1, header = TRUE,colTypes = XLC$DATA_TYPE.STRING)
-
-splist <-  read.csv('C:/Users/ellen.bledsoe/Desktop/Git/PortalData/Plants/Portal_plant_species.csv', as.is = T)
+splist <-  read.csv('Plants/Portal_plant_species.csv', as.is = T)
 
 plots <-  1:24
 transects <- c("T1", "T2")
