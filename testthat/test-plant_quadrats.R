84789650f755253ec03a327662400890e81abf55
Glenda Yenni <glenda@weecology.org>
2017-10-20 14:42:13 -0600

    Allow quadrat abundance to go to 2000

--- testthat/test-plant_quadrats.R
+++ testthat/test-plant_quadrats.R
@@ -31,7 +31,7 @@ test_that("valid species", {
 
 test_that("valid abundance", {
   
-  expect_true(all(data$abundance %in% 0:1000))
+  expect_true(all(data$abundance %in% 0:2000))
 })
 
 test_that("valid cover", {
