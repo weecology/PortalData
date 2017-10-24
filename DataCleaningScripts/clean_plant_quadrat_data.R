844c9878adacf77761455bd30af35db196ddd48d
Renata Diaz <diaz.renatam@gmail.com>
2017-10-24 15:47:19 -0400

    Starting quadrats

--- DataCleaningScripts/clean_plant_quadrat_data.R
+++ DataCleaningScripts/clean_plant_quadrat_data.R
@@ -17,33 +17,173 @@
 #   Section 4
 #     * a. Make sure to check directory for appending the existing file *
 
-library(XLConnect)
+library(openxlsx)
 library(dplyr)
 
-source('compare_raw_data.r')
 
 ######################
 # 1. Load Excel file #
 ######################
 
-season <-  'Winter'
+season <-  'Summer'
 year <-  '2017'
-filepath <-  'C:/Users/ellen.bledsoe/Dropbox/Portal/PORTAL_primary_data/Plant/Quadrats/Dataraw/Newdata/'
+filepath <-  '/Users/renatadiaz/Dropbox/Portal/PORTAL_primary_data/Plant/Quadrats/Dataraw/Newdata/'
 
 newfile <-  paste(filepath, season, year, '.xlsx', sep='')
 
 ##################################
 # 2. Compare double-entered data #
 ##################################
+excel_file = newfile
+# load data from excel workbook
+ws1 = read.xlsx(excel_file, sheet = 1, colNames = TRUE, na.strings = c('', 'NA', ' '))
+ws2 = read.xlsx(excel_file, sheet = 2, colNames = TRUE, na.strings = c('', 'NA', ''))
 
-compare_worksheets(newfile)
+# if the two worksheets are identical, exit function
+if (identical(ws1,ws2)) {                                   
+  print('Worksheets identical')
+} else {
+  unmatched = data.frame(row = c(),column = c())         # empty data frame for storing output
+  num_rows = length(ws1$month)
+  curr_row = 1
+  while (curr_row<=num_rows) {
+    v1 = as.character(as.vector(ws1[curr_row,]))          # extract row from worksheet 1
+    v2 = as.character(as.vector(ws2[curr_row,]))          # extract row from worksheet 2
+    
+    # if the two versions of the row are not identical
+    if (!identical(v1,v2)) {
+      # loop through each element in the row
+      col_error = vector()
+      for (n in seq(length(v1))) {                        
+        if (!identical(v1[n],v2[n])) {
+          # add the column name to output vector
+          col_error = append(col_error,colnames(ws1)[n])
+        }
+      }
+      # append row and column info to output data frame (curr_row+1 to skip header in excel file)
+      unmatched = rbind(unmatched,data.frame(row = curr_row+1,column = col_error))
+    }
+    curr_row = curr_row+1             # increment index and continue loop
+  }
+  
+}
+
+# iterate through mismatches and fix them
+i = 1
+
+i  = i + 1
+unmatched[i, ]
+ws1[unmatched[i, 'row'] -1 , ]
+ws2[unmatched[i, 'row'] - 1, ]
+
+ws2[877, 'species'] <- 'ambr arte'
+ws2[2194, 'abundance'] <- 21
+ws2[1800, 'abundance'] <- 0
+ws2[782, 'abundance'] <- 0
+ws2[753, 'abundance'] <- 0
+ws2[265, 'cover'] <- .1
+ws1[2607, 'species'] <- 'acac palm'
+ws1[2591, 'cover'] <- .1
+ws1[2542, 'cover'] <- 5
+ws1[2541, 'cover'] <- 5
+ws1[2483, 'cf'] <- NA
+ws2[2453, 'abundance'] <- 18
+ws1[2418, 'species'] <- 'kall hirt'
+ws2[2357, 'cover'] <- 1
+ws2[2356, 'cover'] <- 1
+ws1[2294, 'cf'] <- 'dale brac'
+ws2[2292, 'quadrat'] <- 75
+ws1[2290, 'cf'] <- 'dale brac'
+ws1[2282, 'cf'] <- 'dale brac'
+ws1[2279, 'cf'] <- 'dale brac'
+ws1[2272, 'cf'] <- 'dale brac'
+ws1[2264, 'cf'] <- 'dale brac'
+ws2[2257:2262, 'plot'] <- 11
+ws2[2256, 'quadrat'] <- 37
+ws2[2253, 'quadrat'] <- 35
+ws1[2232, 'cover'] <- 1
+ws1[2227, 'cf'] <-'dale brac'
+ws2[2184, 'abundance'] <- 2
+ws1[2181, 'cover'] <- 1
+ws1[2174:2179, 'quadrat'] <- 35
+ws1[2166, 'cf'] <- NA
+ws2[2160:2186, 'plot'] <- 12
+ws1[2148, 'cf'] <- NA
+ws1[2140, 'cf'] <- NA
+ws1[2103, 'cf'] <- NA
+ws1[2100, 'cover'] <- 1
+ws2[2081, 'abundance'] <- 5
+ws2[2074, 'cover'] <- .1
+ws1[2013, 'cf'] <- NA
+ws1[2002, 'cf'] <- NA
+ws1[1982, 'cf'] <- NA
+ws1[1973, 'cf'] <- NA
+ws1[1843, 'cover'] <- 1
+ws1[1805, 'cover'] <- 1
+ws1[1785, 'cf'] <- 'chlo virg erio lemm pani mili'
+ws1[1752, 'cover'] <- 3
+ws1[1751, 'cover'] <- .1
+ws1[1748, 'cover'] <- 1
+ws1[1747, 'cover'] <- 1
+ws1[1725, 'cover'] <- .1
+ws1[1723, 'cover'] <- .1
+ws1[1661, 'cf'] <- NA
+ws2[1658:1660, 'quadrat'] <- 37
+ws2[269, 'cover'] <- .1
+ws2[267, 'cover'] <- .1
+ws2[266, 'cover'] <- .1
+ws2[265, 'cover'] <- .1
+ws2[264, 'cover'] <- .1
+ws2[263, 'cover'] <- .1
+ws2[1609, 'species'] <- 'erag lehm'
+ws2[1589, 'species'] <- 'moll cerv'
+ws2[1531, 'abundance'] <- 5
+ws2[1523, 'cover'] <- 1
+ws2[1502, 'cover'] <- 1
+ws2[1488, 'cover'] <- 1
+ws1[1388, 'cf'] <- NA
+ws2[1362, 'abundance'] <- 27
+ws1[1140, 'cover'] <- 1
+ws2[1119, 'cover'] <- 1
+ws1[1038, 'abundance'] <- 2
+ws1[1025, 'cover'] <- 1
+ws2[992, 'cover'] <- 50
+ws2[991, 'cover'] <- .1
+ws1[974, 'cover'] <- 1
+ws1[967, 'cover'] <- 1
+ws1[964, 'cover'] <- 1
+ws2[951, 'cover'] <- 1
+ws1[857:866, 'plot'] <- 10
+ws1[791, 'abundance'] <- 3
+ws1[601, 'species'] <- 'euph serr'
+ws1[550, 'cover'] <- 1
+ws1[375, 'species'] <- 'euph serp'
+ws1[341, 'cover'] <- 1
+ws1[231, 'abundance'] <- 12
+ws1[213, 'species'] <- 'erag lehm'
+ws2[178, 'abundance'] <- 26
+ws2[144, 'cover'] <- .1
+ws2[142, 'cover'] <- .1
+ws2[142, 'cf'] <- 'dale brac'
+ws2[136, 'abundance'] <- 16
+ws1[122, 'cover'] <- 1
+ws1[87, 'abundance'] <- 1
+ws1[80, 'cf'] <- 'dale brac'
+ws2[41, 'abundance'] <- 64
+ws1[34, 'cf'] <- 'dale brac'
+ws2[30, 'cover'] <- 25
+ws2[30, 'species'] <- 'erag lehm'
+ws1[7, 'cf'] <- 'dale brac'
+
+
+# Save matching datasheet
+write.csv(ws1, '/Users/renatadiaz/Dropbox/Portal/PORTAL_primary_data/Plant/Quadrats/Dataraw/Newdata/Summer2017_matched.csv', row.names = FALSE)
 
 ######################
 # 3. Quality control #
 ######################
 
-wb = loadWorkbook(newfile)
-ws = readWorksheet(wb, sheet = 1, header = TRUE)
+ws = read.csv('/Users/renatadiaz/Dropbox/Portal/PORTAL_primary_data/Plant/Quadrats/Dataraw/Newdata/Summer2017_matched.csv')
 
 splist = read.csv('./Plants/Portal_plant_species.csv',as.is=T)
 
@@ -53,7 +193,13 @@ stakes = c(11,13,15,17,31,33,35,37,51,53,55,57,71,73,75,77)
 # =====================================
 # species names not in official list
 
-unique(ws$species[!(ws$species %in% splist$speciescode)])
+new.names = setdiff(ws$species,splist$speciescode)
+
+# NAs can stay because they will be removed later
+ws[which(is.na(ws$species)), ]
+ws[which(ws$species == new.names[2]), ]
+ws[which(ws$species == new.names[3]), ]
+ws[which(ws$species == new.names[4]), ]
 
 #**ADD valid new species to species list**
 
