cc89023da0fd35330fd8a2998768cc8db20414e7
Ethan White <ethan@weecology.org>
2018-03-29 10:40:31 -0400

    Make a new GitHub release every time the data is updated

--- update_repo.sh
+++ update_repo.sh
@@ -7,3 +7,9 @@ git commit -m "Update supplemental data tables: Travis Build $TRAVIS_BUILD_NUMBE
 
 git remote add deploy https://${GITHUB_TOKEN}@github.com/weecology/PortalData.git > /dev/null 2>&1
 git push --quiet deploy master > /dev/null 2>&1
+
+# Create a new release to trigger Zenodo archiving
+version=$(cat version.txt)
+git tag $version
+git push --quiet deploy --tags > /dev/null 2>&1
+curl -v -i -X POST -H "Content-Type:application/json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/weecology/PortalData/releases -d "{\"tag_name\":\"$version\"}"
