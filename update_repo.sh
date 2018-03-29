3b6c9578648931db2543e0789c0500740179705e
Ethan White <ethan@weecology.org>
2018-03-29 10:42:14 -0400

    Don't run Travis twice
    Don't run Travis on changes generated by Travis. This prevents unnecessarily
    running Travis after data has already been updated and makes errors related to
    extra version updates less likely.

--- update_repo.sh
+++ update_repo.sh
@@ -3,7 +3,7 @@ git config --global user.name "Weecology Deploy Bot"
 
 git checkout master
 git add -u
-git commit -m "Update supplemental data tables: Travis Build $TRAVIS_BUILD_NUMBER"
+git commit -m "Update supplemental data tables: Travis Build $TRAVIS_BUILD_NUMBER [skip ci]"
 
 git remote add deploy https://${GITHUB_TOKEN}@github.com/weecology/PortalData.git > /dev/null 2>&1
 git push --quiet deploy master > /dev/null 2>&1
