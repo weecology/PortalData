27a3d944c2d38b675f255c7c1ef473c2289e3872
Glenda Yenni <glenda@weecology.org>
2019-08-13 16:49:54 -0600

    Update weather overlap [minor]

--- DataCleaningScripts/old_weather_overlap.R
+++ DataCleaningScripts/old_weather_overlap.R
@@ -12,7 +12,7 @@
 # Open raw .dat file of new data
 filepath = "~/Dropbox/Portal/PORTAL_primary_data/Weather/Raw_data/2002_Station/"
 
-metfile = "Met486"
+metfile = "Met488"
 
 rawdata = read.csv(paste(filepath,metfile,'.dat',sep=''),head=F,sep=',',
                    col.names=c('code','year','jday','hour','precipitation','airtemp','RH'))
