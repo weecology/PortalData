78b3dbe9db3f17831c88953747b953d8c5908abe
Glenda Yenni <glenda@weecology.org>
2019-12-10 18:48:25 -0700

    Update get_regional_weather [no version bump]

--- DataCleaningScripts/get_regional_weather.R
+++ DataCleaningScripts/get_regional_weather.R
@@ -15,23 +15,29 @@ portal4sw = read.csv(
   dplyr::rename_all(.funs = tolower) %>%
   dplyr::mutate_all(as.character) %>% 
   dplyr::mutate_at(c("latitude","longitude","elevation","prcp","snow","snwd","tmax","tmin","dapr","dasf",
-                     "mdpr","mdsf","tobs","wt01","wt03","wt04","wt05","wt11"), as.numeric)
+                     "mdpr","mdsf","tobs","wt01","wt03","wt04","wt05","wt11"), as.numeric) %>%
+  dplyr::mutate_at("date",as.Date) %>%
+  dplyr::mutate(day = lubridate::day(date), 
+                month = lubridate::month(date), 
+                year = lubridate::year(date)) %>%
+  dplyr::select(year, month, day, dplyr::everything())
   
 
 sansimon = read.csv(
   'https://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/access/US1AZCH0005.csv') %>%
   dplyr::rename_all(.funs = tolower) %>%
   dplyr::mutate_all(as.character) %>% 
-  dplyr::mutate_at(c("latitude","longitude","elevation","prcp","snow","snwd","wesd","wesf"), as.numeric)
+  dplyr::mutate_at(c("latitude","longitude","elevation","prcp","snow","snwd","wesd","wesf"), as.numeric) %>%
+  dplyr::mutate_at("date",as.Date) %>%
+  dplyr::mutate(day = lubridate::day(date), 
+                month = lubridate::month(date), 
+                year = lubridate::year(date)) %>%
+  dplyr::select(year, month, day, dplyr::everything())
 
 
 #Keep only new data
-all_4sw = read.csv(file = "Weather/Portal4sw_regional_weather.csv",header=T,
-                   colClasses=c("character", rep("integer",3), "character", "integer", 
-                                rep("character",3), "Date"))
-all_sansimon = read.csv(file = "Weather/Sansimon_regional_weather.csv",header=T,
-                        colClasses=c("character", rep("integer",3), "character", "integer", 
-                                     rep("character",3), "Date"))
+all_4sw = read.csv(file = "Weather/Portal4sw_regional_weather.csv",header=T)
+all_sansimon = read.csv(file = "Weather/Sansimon_regional_weather.csv",header=T)
 
 new_4sw = dplyr::setdiff(portal4sw,all_4sw)
 new_sansimon = dplyr::setdiff(sansimon,all_sansimon)
