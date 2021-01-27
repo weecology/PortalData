d0389d58fe5eae368ab75d1a7d7424dbaec995ba
Glenda Yenni <glenda@weecology.org>
2021-01-27 14:44:08 -0700

    Hopefully htmltab is in cache [no version bump]

--- install-packages.R
+++ install-packages.R
@@ -9,4 +9,3 @@ pacman::p_load(stringr, Hmisc, dplyr, git2r, openxlsx, lubridate, lunar, jsonlit
                sp, sqldf, raster, RCurl, EML, testthat, htmltab, zoo, tidyr, semver, yaml)
 
 pacman::p_load_gh("16EAGLE/getSpatialData")
-pacman::p_load_gh("crubba/htmltab")
