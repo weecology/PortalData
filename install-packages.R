2d9b1af56e8e47c35f97d3034d877e5afd0ab178
Glenda <gmyenni@users.noreply.github.com>
2021-02-20 15:31:26 -0700

    [patch]

--- install-packages.R
+++ install-packages.R
@@ -2,8 +2,9 @@
 
 if ("pacman" %in% rownames(installed.packages()) == FALSE) install.packages("pacman")
 
-install.packages("RCurl")
-install.packages("curl")
+install.packages("RCurl", type="source", repos="https://cran.rstudio.com")
+install.packages("curl", type="source", repos="https://cran.rstudio.com")
+install.packages("stringi", type="source", repos="https://cran.rstudio.com")
 # Install analysis packages using pacman
 
 pacman::p_load(units, sf, shiny, stringi, stringr, Hmisc, dplyr, git2r, openxlsx, lubridate, lunar, jsonlite, 
