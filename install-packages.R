523f052d2a0490d640d7840fc29b0418e8a719e4
Glenda <gmyenni@users.noreply.github.com>
2021-02-22 15:00:14 -0700

    Setup github actions [no version bump] (#396)

--- install-packages.R
+++ install-packages.R
@@ -2,11 +2,10 @@
 
 if ("pacman" %in% rownames(installed.packages()) == FALSE) install.packages("pacman")
 
-
 # Install analysis packages using pacman
 
-pacman::p_load(stringr, Hmisc, dplyr, git2r, openxlsx, lubridate, lunar, jsonlite, devtools,
-               sp, sqldf, raster, RCurl, EML, testthat, htmltab, zoo, tidyr, semver, yaml)
+pacman::p_load(units, sf, rgdal, lwgeom, shiny, stringi, stringr, Hmisc, dplyr, git2r, openxlsx, lubridate, lunar, jsonlite, 
+               devtools, sp, sqldf, raster, RCurl, EML, testthat, zoo, tidyr, semver, yaml)
 
 pacman::p_load_gh("16EAGLE/getSpatialData")
 devtools::install_url('https://cran.r-project.org/src/contrib/Archive/htmltab/htmltab_0.7.1.1.tar.gz')
