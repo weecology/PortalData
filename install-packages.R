8f3e1054bd46f02f33a5b7677c6936edd8aed779
Glenda <gmyenni@users.noreply.github.com>
2021-02-22 16:45:03 -0700

    Update main.yml (#397)

--- install-packages.R
+++ install-packages.R
@@ -2,18 +2,12 @@
 
 if ("pacman" %in% rownames(installed.packages()) == FALSE) install.packages("pacman")
 
-install.packages("RCurl", type="source", repos="https://cran.rstudio.com")
-install.packages("curl", type="source", repos="https://cran.rstudio.com")
-install.packages("stringi", type="source", repos="https://cran.rstudio.com")
-
-install.packages("sf", type="source", repos="https://cran.rstudio.com")
-install.packages("rgdal", type="source", repos="https://cran.rstudio.com")
-install.packages("lwgeom", type="source", repos="https://cran.rstudio.com")
+#install.packages(c("RCurl","curl","sf","rgdal","lwgeom"), type="source", repos="https://cran.rstudio.com")
 
 # Install analysis packages using pacman
 
-pacman::p_load(units, sf, rgdal, lwgeom, shiny, stringi, stringr, Hmisc, dplyr, git2r, openxlsx, lubridate, lunar, jsonlite, 
-               devtools, sp, sqldf, raster, RCurl, EML, testthat, zoo, tidyr, semver, yaml)
+pacman::p_load(units, sf, rgdal, lwgeom, shiny, stringi, stringr, Hmisc, dplyr, git2r, openxlsx, lubridate, 
+               lunar, jsonlite, devtools, sp, sqldf, raster, RCurl, EML, testthat, zoo, tidyr, semver, yaml)
 
 pacman::p_load_gh("16EAGLE/getSpatialData")
 devtools::install_url('https://cran.r-project.org/src/contrib/Archive/htmltab/htmltab_0.7.1.1.tar.gz')
