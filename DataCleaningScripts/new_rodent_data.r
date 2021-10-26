4c700fd7a60b7fa7ba27c0804b63047fbf938159
Pat Dumandan <patriciakayeduma@u.boisestate.edu>
2021-10-26 19:29:17 -0400

    adding rodents_498[minor]

--- DataCleaningScripts/new_rodent_data.r
+++ DataCleaningScripts/new_rodent_data.r
@@ -14,7 +14,7 @@ source('DataCleaningScripts/clean_pit_tags.R')
 # New file to be checked
 ##############################################################################
 
-newperiod = '497'
+newperiod = '498'
 filepath = "D:\\Dropbox\\Portal\\PORTAL_primary_data\\Rodent\\Raw_data\\New_data"
 
 newfile = paste(filepath, '\\newdat', newperiod, '.xlsx', sep = '')
