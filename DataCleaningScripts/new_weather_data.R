0a30dcf9aff149c7291136b2a6293bddfbcb8295
Glenda <gmyenni@users.noreply.github.com>
2022-06-30 13:34:12 -0600

    Add rodents 506, including a patch to let new_moon_numbers run (#447) [minor]
    * add rodents_506 [minor]
    
    * remove last moon date
    
    Co-authored-by: Renata Diaz <diaz.renatam@gmail.com>
    Co-authored-by: Pat Dumandan <patriciakayeduma@u.boisestate.edu>

--- DataCleaningScripts/new_weather_data.R
+++ DataCleaningScripts/new_weather_data.R
@@ -20,22 +20,22 @@ new_met_data <- function() {
   #httr::set_config(httr::timeout(seconds = 120))
   
 # To read from .dat file
-# header=read.table("~/Dropbox (UFL)/Portal/PORTAL_primary_data/Weather/Raw_data/2016_Station/504_CR1000_MET.dat",
+# header=read.table("~/Dropbox (UFL)/Portal/PORTAL_primary_data/Weather/Raw_data/2016_Station/501_CR1000_MET.dat",
 #                    skip = 1, nrow = 1, header = FALSE, sep=",", stringsAsFactors = FALSE)
 # 
-# rawdata=read.table("~/Dropbox (UFL)/Portal/PORTAL_primary_data/Weather/Raw_data/2016_Station/504_CR1000_MET.dat",
+# rawdata=read.table("~/Dropbox (UFL)/Portal/PORTAL_primary_data/Weather/Raw_data/2016_Station/501_CR1000_MET.dat",
 #                    skip = 4, header = FALSE,sep=",") %>%
 #   `colnames<-`(header) %>%
 #   dplyr::rename(airtemp=AirTC_Avg,precipitation=Rain_mm_Tot,timestamp=TIMESTAMP,record=RECORD,battv=BattV)
 # 
-# header_storms=read.table("~/Dropbox (UFL)/Portal/PORTAL_primary_data/Weather/Raw_data/2016_Station/504_CR1000_storms.dat",
+# header_storms=read.table("~/Dropbox (UFL)/Portal/PORTAL_primary_data/Weather/Raw_data/2016_Station/501_CR1000_storms.dat",
 #                   skip = 1, nrow = 1, header = FALSE, sep=",", stringsAsFactors = FALSE)
 # 
-# stormsnew=read.table("~/Dropbox (UFL)/Portal/PORTAL_primary_data/Weather/Raw_data/2016_Station/504_CR1000_storms.dat",
+# stormsnew=read.table("~/Dropbox (UFL)/Portal/PORTAL_primary_data/Weather/Raw_data/2016_Station/501_CR1000_storms.dat",
 #                    skip = 4, header = FALSE,sep=",") %>%
 #   `colnames<-`(header_storms) %>%
 #   dplyr::rename(precipitation=Rain_mm_Tot,timestamp=TIMESTAMP,record=RECORD,battv=BattV_Min)
-
+  
 today <- lubridate::ymd_hms(gsub(":\\d+:\\d+",":00:00",Sys.time()))
   # Pull raw data (latest week of records, plus some overlap for saftey) & rename columns
   message("Pulling raw weather data")
