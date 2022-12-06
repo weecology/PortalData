37fcd8f3e5dbc54f93cf388dd0313bdda413c1a2
Glenda <gmyenni@users.noreply.github.com>
2022-12-06 12:39:16 -0700

    [minor]

--- install-packages.R
+++ install-packages.R
@@ -10,7 +10,7 @@ install.packages('terra', repos='https://rspatial.r-universe.dev')
 # Install analysis packages using pacman
 
 # pacman::p_load(units, sf, rgdal, lwgeom, shiny, stringi, stringr, Hmisc, dplyr, git2r, openxlsx, lubridate, htmltab, 
-               lunar, jsonlite, devtools, sp, sqldf, raster, RCurl, EML, testthat, zoo, tidyr, semver, yaml)
+#                lunar, jsonlite, devtools, sp, sqldf, raster, RCurl, EML, testthat, zoo, tidyr, semver, yaml)
                
 
 pacman::p_load(units, shiny, dplyr, openxlsx, lubridate, htmltab, 
