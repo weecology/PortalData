ce7de25a0a65632364587bf5eb80ed9d60f4d4e3
Glenda <gmyenni@users.noreply.github.com>
2022-12-06 13:45:45 -0700

    Lib and package installs [minor] (#474)

--- install-packages.R
+++ install-packages.R
@@ -2,13 +2,16 @@
 
 if ("pacman" %in% rownames(installed.packages()) == FALSE) install.packages("pacman")
 
-#remove.packages("stringi")
-#remove.packages("stringr")
-#install.packages(c("stringi","stringr","RCurl","curl","Hmisc", "EML", "sf","rgdal","lwgeom", "git2r"), type="source", repos="https://cran.rstudio.com")
+remove.packages("stringi")
+remove.packages("stringr")
+install.packages(c("stringi","stringr","RCurl","curl","Hmisc", "EML", "sf","rgdal","lwgeom", "git2r"), type="source", repos="https://cran.rstudio.com")
+install.packages('terra', repos='https://rspatial.r-universe.dev')
 
 # Install analysis packages using pacman
 
-pacman::p_load(units, sf, rgdal, lwgeom, shiny, stringi, stringr, Hmisc, dplyr, git2r, openxlsx, lubridate, htmltab, 
-               lunar, jsonlite, devtools, sp, sqldf, raster, RCurl, EML, testthat, zoo, tidyr, semver, yaml)
+# pacman::p_load(units, sf, rgdal, lwgeom, shiny, stringi, stringr, Hmisc, dplyr, git2r, openxlsx, lubridate, htmltab, 
+#                lunar, jsonlite, devtools, sp, sqldf, raster, RCurl, EML, testthat, zoo, tidyr, semver, yaml)
+               
 
-pacman::p_load_gh("16EAGLE/getSpatialData")
+pacman::p_load(units, shiny, dplyr, openxlsx, lubridate, htmltab, 
+               lunar, jsonlite, devtools, sp, sqldf, raster, testthat, zoo, tidyr, semver, yaml)
