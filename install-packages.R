1a1878a1ba428d0a580f7a080a816a1e75b05739
Glenda Yenni <glenda@weecology.org>
2023-02-28 12:45:37 -0700

    Switch to terraa for rasters [no version bump]

--- install-packages.R
+++ install-packages.R
@@ -1,14 +1,22 @@
-install.packages(c("devtools", "dplyr", "EML", "git2r", "htmltab", "jsonlite", "lubridate", "lunar", "openxlsx", "raster", 
-                   "semver", "shiny", "sp", "sqldf", "terra", "testthat", "tidyr", "units", "yaml", "zoo"))
-
-# install.packages(c("Rcpp","stringi","stringr","RCurl","curl","Hmisc", "EML", "sf","rgdal","lwgeom", "git2r"), type="source", repos="https://cran.rstudio.com")
-# install.packages('terra', repos='https://rspatial.r-universe.dev')
-
-# Install analysis packages using pacman
-
-# pacman::p_load(units, sf, rgdal, lwgeom, shiny, stringi, stringr, Hmisc, dplyr, git2r, openxlsx, lubridate, htmltab, 
-#                lunar, jsonlite, devtools, sp, sqldf, raster, RCurl, EML, testthat, zoo, tidyr, semver, yaml)
-               
-
-# pacman::p_load(units, shiny, dplyr, openxlsx, lubridate, htmltab, 
-#                lunar, jsonlite, devtools, sp, sqldf, raster, testthat, zoo, tidyr, semver, yaml)
+install.packages(c("devtools", 
+                   "dplyr", 
+                   "EML", 
+                   "git2r", 
+                   "htmltab", 
+                   "jsonlite", 
+                   "lubridate", 
+                   "lunar", 
+                   "magrittr",
+                   "openxlsx", 
+                   "semver", 
+                   "shiny", 
+                   "sp", 
+                   "sqldf",
+                   "stringi",
+                   "stringr",
+                   "terra", 
+                   "testthat", 
+                   "tidyr", 
+                   "units", 
+                   "yaml", 
+                   "zoo"))
