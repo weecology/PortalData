ae1458eb1920ef9ad7c55446d1aa70e59170a3ea
Pat Dumandan <patriciakayeduma@u.boisestate.edu>
2023-10-23 13:17:36 -0400

    add winter 2023 plant census data [minor]

--- DataCleaningScripts/clean_plant_quadrat_data.R
+++ DataCleaningScripts/clean_plant_quadrat_data.R
@@ -26,8 +26,8 @@ source('DataCleaningScripts/plant_data_cleaning_functions.R')
 # 1. Load Excel file #
 ######################
 
-season <-  'Summer'
-year <-  '2022'
+season <-  'Winter'
+year <-  '2023'
 filepath <-  'D:\\Dropbox\\Portal\\PORTAL_primary_data\\Plant\\Quadrats\\Dataraw\\Newdata\\'
 
 newfile <-  paste(filepath, season, year, '.xlsx', sep='')
