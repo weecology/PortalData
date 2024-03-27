6b5941afaaa53180a3993fc97b443e5bba41d1fc
Pat Dumandan <patriciakayeduma@u.boisestate.edu>
2024-03-27 07:18:57 -0400

    add rodent census 527 data [minor]

--- DataCleaningScripts/new_rodent_data.r
+++ DataCleaningScripts/new_rodent_data.r
@@ -15,7 +15,7 @@ source('DataCleaningScripts/clean_pit_tags.R')
 # New file to be checked
 ##############################################################################
 
-newperiod = '526'
+newperiod = '527'
 
 filepath = "D:\\Dropbox\\Portal\\PORTAL_primary_data\\Rodent\\Raw_data\\New_data"
 
