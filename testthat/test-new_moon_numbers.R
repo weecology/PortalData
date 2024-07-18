468e13e37e555e855cadd5a9531a659fadb08cf0
Patricia Kaye Dumandan <46734193+patdumandan@users.noreply.github.com>
2024-07-18 18:14:38 -0400

    add moon_dates test [minor] (#534)
    * add test to ensure all census periods are assigned a newmoon number
    * Add moondate for census 506
    ---------
    Co-authored-by: Glenda Yenni <glenda@weecology.org>

--- testthat/test-new_moon_numbers.R
+++ testthat/test-new_moon_numbers.R
@@ -2,6 +2,7 @@ context("checks that new moon numbers are being added correctly")
 
 moon_dates <- read.csv("../Rodents/moon_dates.csv",header=T,
                        colClasses=c("integer", "Date", "integer", "Date"))
+trappingdat <- read.csv("../Rodents/Portal_rodent_trapping.csv") 
 
 test_that("no moondates are skipped or duplicated", {
   
@@ -12,10 +13,14 @@ test_that("no moondates are skipped or duplicated", {
   expect_false(any(duplicated(moon_dates$newmoondate, incomparables = NA)))
 })
 
-
 test_that("no periods skipped or duplicated", {
-
+  
   expect_true(all(diff(moon_dates$period)==1,na.rm=T))
   expect_false(any(duplicated(moon_dates$period, incomparables = NA)))
   expect_false(any(duplicated(moon_dates$censusdate, incomparables = NA)))
 })
+
+test_that("all periods have newmoonnumbers assigned", {
+  
+  expect_true(all(unique(trappingdat$period)==moon_dates$period[!is.na(moon_dates$period)]))
+  })
