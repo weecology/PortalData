3eaa550c9c4443f80c66674748b9abd1406db3be
Glenda <gmyenni@users.noreply.github.com>
2025-12-09 13:27:02 -0700

    Disable NDVI update and cleanup commands [minor]
    Comment out NDVI update section in update-data.R. 
    USGS API has changed and is causing action to time out. Temporarily disabling NDVI updating until resolved.

--- update-data.R
+++ update-data.R
@@ -16,13 +16,13 @@ source("DataCleaningScripts/new_moon_numbers.r"); writenewmoons()
 message("Updating Plant census table")
 source("DataCleaningScripts/update_portal_plant_censuses.R"); writecensustable()
 
-message("Updating NDVI")
-output <- system2("python3", "DataCleaningScripts/NDVI.py", stderr = TRUE, stdout = TRUE)
-# Check if the Python script returns an error
-if (!is.null(attr(output, "status")) && attr(output, "status") != 0) {
-stop("Error in NDVI.py:\n", paste(output, collapse = "\n"))
-}
-message("NDVI update completed successfully.")
+# message("Updating NDVI")
+# output <- system2("python3", "DataCleaningScripts/NDVI.py", stderr = TRUE, stdout = TRUE)
+# # Check if the Python script returns an error
+# if (!is.null(attr(output, "status")) && attr(output, "status") != 0) {
+# stop("Error in NDVI.py:\n", paste(output, collapse = "\n"))
+# }
+# message("NDVI update completed successfully.")
 
-source("DataCleaningScripts/update_ndvi.R"); writendvitable()
-system("rm -r ./NDVI/landsat-data ./NDVI/scenes.csv")
+# source("DataCleaningScripts/update_ndvi.R"); writendvitable()
+# system("rm -r ./NDVI/landsat-data ./NDVI/scenes.csv")
