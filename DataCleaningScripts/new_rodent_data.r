55410007ab24cc70b3fda2bd25e9c13b0fd65e20
Sam Lamb <s.lamb@ufl.edu>
2025-12-10 16:51:57 -0500

    Add rodent census 541 data [minor]

--- DataCleaningScripts/new_rodent_data.r
+++ DataCleaningScripts/new_rodent_data.r
@@ -15,7 +15,7 @@ source('DataCleaningScripts/clean_pit_tags.R')
 # New file to be checked
 ##############################################################################
 
-newperiod = '540'
+newperiod = '541'
 
 filepath = "~/Dropbox/Portal/PORTAL_primary_data/Rodent/Raw_data/New_data"
 
