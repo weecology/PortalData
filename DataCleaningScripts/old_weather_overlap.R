7a4b9ff41a3446dd7b1b66b10d825f46e87b8ca3
Glenda Yenni <glenda@weecology.org>
2026-08-25 13:44:33 -0600

    Add 554 old weather overlap [minor]

--- DataCleaningScripts/old_weather_overlap.R
+++ DataCleaningScripts/old_weather_overlap.R
@@ -12,7 +12,7 @@ library(dplyr)
 # Open raw .dat file of new data
 filepath = "~/Dropbox (UFL)/Portal/PORTAL_primary_data/Weather/Raw_data/2002_Station/"
 
-metfile <- "Met551"
+metfile <- "Met554"
 
 rawdata <- read.csv(paste(filepath,metfile,'.dat',sep=''),head=F,sep=',', 
                    col.names=c('code','year','jday','hour','precipitation','airtemp','RH'))
